c1ab759281c870179a1ecad7faa7a04a4a9e3c1f
connor_boyce <cpboycercf@gmail.com>
2019-08-21 21:54:13 -0400

    Added local data storage so that items are saved upon terminating the application

--- ToDoList/ToDoListViewController.swift
+++ ToDoList/ToDoListViewController.swift
@@ -11,9 +11,14 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggos", "Get to upside down"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
+            itemArray = items
+        }
         // Do any additional setup after loading the view.
     }
     
@@ -54,6 +59,7 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen once the user clicks the add item button
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
             
         }
